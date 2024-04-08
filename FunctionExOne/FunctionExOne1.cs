881e041fd028bfcdd9b2ae6977329d10338a7af1
liront212 <lirontabib212@gmail.com>
2024-04-08 19:19:02 +0300

    new file

---
+++ FunctionExOne/FunctionExOne1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FunctionExOne.Abstract;
+
+namespace FunctionExOne
+{
+    public class FunctionExOne1 : IExercise
+    {
+        public int GetLastDigit(int number)
+        {
+            return (number % 10);
+        }
+
+        public int GetAllButLastDigit(int number)
+        {
+            return (number /= 10);
+        }
+
+        public int GetAmountOfDigits(int number)
+        {
+            int count = 0;
+            if (number == 0)
+            {
+                return 1;
+            }
+            while (number != 0)
+            {
+                number /= 10;
+                count++;
+            }
+            return count;
+        }
+
+        public int GetDigintInIndex(int number, int index)
+        {
+            string num = number.ToString();
+            return num[index];
+        }
+    }
+}
